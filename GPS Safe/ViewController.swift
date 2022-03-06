342e2d9ac6934636ce5cad198414583ca3fa5396
Wojtek <wojteklag@gmail.com>
2022-03-06 16:16:42 +0100

    Initial Commit

---
+++ GPS Safe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GPS Safe
+//
+//  Created by Wojtek on 06/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
