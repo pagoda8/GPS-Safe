1ff87244dda72cfb6ec0f7880944a14f10e60ddb
Wojtek <wojteklag@gmail.com>
2022-03-13 22:23:52 +0100

    Made a login screen

---
+++ GPS Safe/LoginViewController.swift
@@ -0,0 +1,40 @@
+//
+//  LoginViewController.swift
+//  GPS Safe
+//
+//  Created by Wojtek on 13/03/2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+	
+	@IBOutlet weak var username: UITextField!
+	@IBOutlet weak var password: UITextField!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		// Do any additional setup after loading the view.
+	}
+	
+	@IBAction func signupTapped(_ sender: Any) {
+		
+	}
+	
+	@IBAction func loginTapped(_ sender: Any) {
+		
+	}
+	
+	
+	/*
+	 // MARK: - Navigation
+	 
+	 // In a storyboard-based application, you will often want to do a little preparation before navigation
+	 override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+	 // Get the new view controller using segue.destination.
+	 // Pass the selected object to the new view controller.
+	 }
+	 */
+	
+}
