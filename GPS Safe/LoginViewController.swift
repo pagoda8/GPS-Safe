69747413e49614d17b04e2763cdb892ed96f5d08
Wojtek <wojteklag@gmail.com>
2022-03-25 20:31:39 +0100

    Made login and signup alerts

--- GPS Safe/LoginViewController.swift
+++ GPS Safe/LoginViewController.swift
@@ -19,11 +19,47 @@ class LoginViewController: UIViewController {
 	}
 	
 	@IBAction func signupTapped(_ sender: Any) {
-		
+		if (validFields()) {
+			signup()
+		}
 	}
 	
 	@IBAction func loginTapped(_ sender: Any) {
+		if (validFields()) {
+			login()
+		}
+	}
+	
+	func validFields() -> Bool {
+		if (username.text?.isEmpty == true) {
+			let alert = UIAlertController(title: "Username missing", message: "Please input a username", preferredStyle: UIAlertController.Style.alert)
+			alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+			self.present(alert, animated: true, completion: nil)
+			return false
+		}
 		
+		if (password.text?.isEmpty == true) {
+			let alert = UIAlertController(title: "Password missing", message: "Please input a password", preferredStyle: UIAlertController.Style.alert)
+			alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+			self.present(alert, animated: true, completion: nil)
+			return false
+		}
+		
+		return true
+	}
+	
+	func signup() {
+		//Check if username already exists, if so show alert
+		//Create account in db
+		//Show success alert and ask to log in
+	}
+	
+	func login() {
+		//Try to log in, if success go to MySafe //Later make global var for username
+		//Else show alert
+		let vc = self.storyboard?.instantiateViewController(withIdentifier: "mySafe")
+		vc?.modalPresentationStyle = .overFullScreen
+		self.present(vc!, animated: true)
 	}
 	
 	
