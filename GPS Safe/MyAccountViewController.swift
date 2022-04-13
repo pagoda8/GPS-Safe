26f17610a3874a80f3d6f03bc5cf9a5dcdf1f2df
Wojtek <wojteklag@gmail.com>
2022-04-13 20:01:47 +0100

    Added username label and log out button to My Account screen

--- GPS Safe/MyAccountViewController.swift
+++ GPS Safe/MyAccountViewController.swift
@@ -8,11 +8,23 @@
 import UIKit
 
 class MyAccountViewController: UIViewController {
+	
+	@IBOutlet weak var currentUser: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+		
+		currentUser.text = "Username: " + AppDelegate.get().currentUser
 
         // Do any additional setup after loading the view.
     }
-
+	
+	@IBAction func logout(_ sender: Any) {
+		AppDelegate.get().currentUser = ""
+		
+		let vc = self.storyboard?.instantiateViewController(withIdentifier: "login")
+		vc?.modalPresentationStyle = .overFullScreen
+		self.present(vc!, animated: true)
+	}
+	
 }
